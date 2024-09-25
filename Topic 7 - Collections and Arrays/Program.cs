342f317e289e983ca476c10a94391154ea055197
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-25 09:39:18 -0400

    Class ended

--- Topic 7 - Collections and Arrays/Program.cs
+++ Topic 7 - Collections and Arrays/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random generator = new Random();
+            int[] numbers = new int[30];
+
+            int evenNumberAmount = 0;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                numbers[i - 1] = generator.Next(1, 101);
+                Console.Write(numbers[i - 1] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                if ((numbers[i - 1] % 2) == 0)
+                    evenNumberAmount++;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("There are " + evenNumberAmount + " even numbers in the array!");
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                if ((numbers[i - 1] % 5) == 0)
+                    Console.Write(numbers[i - 1] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                if ((i % 2) != 0)
+                    Console.Write(numbers[i - 1] + " ");
+            }
         }
     }
 }
