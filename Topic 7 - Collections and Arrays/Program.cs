fbd2bbeab25ec643fc0f60fd932b2a338820eb5d
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-25 09:14:19 -0400

    Add project files.

---
+++ Topic 7 - Collections and Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7___Collections_and_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
